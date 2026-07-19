a956074cd2b2306285bbd1ccbf05a3a172204782
Arjun <a.puri@uwzorgonline.nl>
2026-07-19 17:07:41 +0200

    Add fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,25 @@
 import type { Metadata } from 'next';
-import { Raleway, Montserrat } from 'next/font/google';
+import { Bebas_Neue, Quicksand } from 'next/font/google';
 import '../styles/globals.css';
 
-const raleway = Raleway({
-  variable: '--font-raleway',
+const bebasNeue = Bebas_Neue({
+  variable: '--font-bebas',
+  subsets: ['latin'],
+  fallback: ['system-ui', 'arial'],
+  display: 'swap',
+  weight: '400',
+});
+
+const quicksand = Quicksand({
+  variable: '--font-quicksand',
   subsets: ['latin'],
   fallback: ['system-ui', 'arial'],
   display: 'swap',
 });
 
-const montserrat = Montserrat({
-  variable: '--font-montserrat',
-  subsets: ['latin'],
-  fallback: ['system-ui', 'arial'],
-  display: 'swap',
-});
-
+// TODO: Figure out how to localise metadata - Use a generateMetaData function to pull SEO from sanity
 export const metadata: Metadata = {
-  title: 'Next.js Starter App',
+  title: 'Banh Mi Oi!',
   description: 'This is a starter app for Next.js to get started faster for projects.',
 };
 
@@ -28,7 +30,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${raleway.variable} ${montserrat.variable} bg-background text-foreground antialiased`}>
+      <body className={`${bebasNeue.variable} ${quicksand.variable} bg-background text-foreground antialiased`}>
         <div className="flex min-h-screen flex-col">
           <main className="flex-1">{children}</main>
         </div>
