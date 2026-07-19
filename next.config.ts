131567f22cd1bae73468a4ce35eabf94477ff419
Arjun <a.puri@uwzorgonline.nl>
2026-07-19 20:19:10 +0200

    Add sanity image loader

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from 'next';
 import path from 'path';
 
 const nextConfig: NextConfig = {
+    images: {
+        loader: 'custom',
+        loaderFile: './src/sanity/lib/image-loader.ts',
+        deviceSizes: [480, 768, 1024, 1440, 1920],
+    },
     turbopack: {
         root: path.join(__dirname, '..'),
     },
