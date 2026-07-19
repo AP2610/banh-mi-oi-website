ecd80633ef6066385b466f65d97d399b8648c39e
Arjun <a.puri@uwzorgonline.nl>
2026-07-19 18:44:56 +0200

    Update structure

--- src/sanity/structure.ts
+++ src/sanity/structure.ts
@@ -1,4 +1,25 @@
 import type { StructureResolver } from 'sanity/structure';
 
+const singletonTypes = new Set(['homePage']);
+
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
-export const structure: StructureResolver = (S) => S.list().title('Content').items(S.documentTypeListItems());
+export const structure: StructureResolver = (S) =>
+    S.list()
+        .title('Content')
+        .items([
+            S.listItem()
+                .id('pages')
+                .title('Pages')
+                .child(
+                    S.list()
+                        .title('Pages')
+                        .items([
+                            S.listItem()
+                                .id('homePage')
+                                .title('Homepage')
+                                .schemaType('homePage')
+                                .child(S.document().schemaType('homePage').documentId('homePage').title('Homepage')),
+                            ...S.documentTypeListItems().filter((listItem) => !singletonTypes.has(listItem.getId() ?? '')),
+                        ]),
+                ),
+        ]);
