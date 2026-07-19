750b91ffe662b439c4ebf61bd784e41dd2f5868d
Arjun <a.puri@uwzorgonline.nl>
2026-07-19 20:35:01 +0200

    Use generic query

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,9 +1,13 @@
-import { Hero } from '@/components/features/hero';
-import { getHomePageHero } from '@/sanity/content/home-page';
+import { Hero, type HomePageHero } from '@/components/features/hero';
+import { sanityFetch } from '@/sanity/lib/fetch';
+import { HOME_PAGE_QUERY } from '@/sanity/queries/home-page';
 
 // TODO: Add CTAs and copy to hero schema
 const HomePage = async () => {
-    const hero = await getHomePageHero('fr');
+    const hero = await sanityFetch<HomePageHero, { locale: 'fr' }>({
+        query: HOME_PAGE_QUERY,
+        params: { locale: 'fr' },
+    });
 
     return <Hero hero={hero} />;
 };
