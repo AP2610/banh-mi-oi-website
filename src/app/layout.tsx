551bcf14632e4a74ada306a2154b6f2d683a2da0
Arjun <a.puri@uwzorgonline.nl>
2026-07-25 20:22:32 +0200

    feat(metadata): install favicon and web app manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,14 @@ interface RootLayoutProps {
 const RootLayout = ({ children }: RootLayoutProps) => {
     return (
         <html lang="fr">
+            <head>
+                <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+                <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+                <link rel="shortcut icon" href="/favicon.ico" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <meta name="apple-mobile-web-app-title" content="Banh Mi Oi" />
+                <link rel="manifest" href="/site.webmanifest" />
+            </head>
             <body className={`${bebasNeue.variable} ${quicksand.variable}`}>{children}</body>
         </html>
     );
